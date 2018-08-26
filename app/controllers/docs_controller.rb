ce5fd62fdca98ae57958eac219128078563120d0
Brian Peguero <brianpeguero@gmail.com>
2018-08-26 16:46:33 -0400

    Added the show and index view file also added to their actions

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,15 +1,27 @@
 class DocsController < ApplicationController
+#allows it to run before other actions
+    before_action :find_doc, only: [:show, :edit, :update, :destroy]
 
     def index
+        #in order of created new to old
+        @doc = Doc.all.order("created_at DESC")
     end
 
     def show
     end
 
     def new
+        @doc = Doc.new
     end
 
     def create
+        @doc = Doc.new(doc_params)
+
+        if @doc.save
+            redirect_to @doc
+        else
+            render "new"
+        end
     end
 
     def edit
@@ -23,8 +35,10 @@ class DocsController < ApplicationController
 
     private
         def find_doc
+            @doc = Doc.find(params[:id])
         end
 
         def doc_params
+            params.require(:doc).permit(:title, :content)
         end
 end
